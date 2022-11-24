4bb96b35220661f8e0070356f802006a40a72f6c
Frid Zina <friday.zina@gmail.com>
2022-11-25 00:56:55 +0300

    #4 Добавлена возвожность конвертировать в .jpg

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ if __name__ == '__main__':
         print("Введите полный путь к папке с изображениями:")
     path_link = input()
 
+    print("В какой формат вы хотите конвертировать: png или jpg? [png/jpg]")
+    conv_to = input()
+
     re_value = -1
     print("Хотите изменить размер изображения? [y/n]")
     resize = input()
@@ -17,5 +20,5 @@ if __name__ == '__main__':
         print("Введите желаемый процент сжатия/расширения. Например, 50 или 200")
         re_value = int(input())
 
-    converter = Converter(path_link, one_many, re_value)
+    converter = Converter(path_link, one_many, re_value, conv_to)
     converter.convert()
